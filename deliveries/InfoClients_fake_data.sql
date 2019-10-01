eb37006f684284a0f3cf9f69ea3d1ca6483938fe
Jorge Madrigal <jmadrigal.webmaster@gmail.com>
2019-09-30 22:19:19 -0500

    feat: add fake data on sql file

---
+++ deliveries/InfoClients_fake_data.sql
@@ -0,0 +1,18 @@
+INSERT INTO `cities` (`id`, `name`, `state_id`, `createdAt`, `updatedAt`) VALUES
+(1,	'Jacksonville',	1,	'2019-10-01 00:13:28',	'2019-10-01 00:13:28'),
+(2,	'Miami',	1,	'2019-10-01 00:13:43',	'2019-10-01 00:13:43'),
+(3,	'Tampa',	1,	'2019-10-01 00:14:05',	'2019-10-01 00:14:05'),
+(4,	'Nevada',	2,	'2019-10-01 00:16:30',	'2019-10-01 00:16:30'),
+(5,	'Orange',	2,	'2019-10-01 00:16:43',	'2019-10-01 00:16:43');
+
+INSERT INTO `countries` (`id`, `name`, `createdAt`, `updatedAt`) VALUES
+(1,	'USA',	'2019-10-01 00:12:36',	'2019-10-01 00:12:36');
+
+INSERT INTO `sales_representatives` (`id`, `name`, `email`, `createdAt`, `updatedAt`) VALUES
+(1,	'Jorge Madrigal',	'sales1@gmail.com',	'2019-10-01 00:17:10',	'2019-10-01 00:17:10'),
+(2,	'Jose Madrigal',	'sales2@gmail.com',	'2019-10-01 00:17:40',	'2019-10-01 00:17:40');
+
+
+INSERT INTO `states` (`id`, `name`, `country_id`, `createdAt`, `updatedAt`) VALUES
+(1,	'Florida',	1,	'2019-10-01 00:13:00',	'2019-10-01 00:13:00'),
+(2,	'California',	1,	'2019-10-01 00:15:50',	'2019-10-01 00:15:50');
